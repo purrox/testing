d5ac7f28ea003439cdf624b8f45fdf70ce7f83ca
purro <pcass78@gmail.com>
2020-11-17 14:50:24 -0600

    added await

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -1,22 +1,22 @@
 describe('Outlook sign in and addi n', () => {
-  it('can find search results', async () => {
-      await browser.url('https://login.live.com/');
-      await $('input[name="loginfmt"]').setValue('ca.salas@outlook.com');
-      await $('#idSIButton9').click();
-      await $('input[name="passwd"]').setValue('ocle32008');
-      await $('#idSIButton9').click();
-      await $('#idSIButton9').click();
+  it('can find search results', () => {
+      browser.url('https://login.live.com/');
+      $('input[name="loginfmt"]').setValue('ca.salas@outlook.com');
+      $('#idSIButton9').click();
+      $('input[name="passwd"]').setValue('ocle32008');
+      $('#idSIButton9').click();
+      $('#idSIButton9').click();
       browser.url('https://outlook.live.com/calendar/');
-      await $('#NewEventButtonAnchor').click();
-      await $('.ms-Link=More options').click();
-      await $('.ms-Button--commandBar[aria-label="More options"]').click();
-      await $('button[name="Get Add-ins"]').click();
+      $('#NewEventButtonAnchor').click();
+      $('.ms-Link=More options').click();
+      $('.ms-Button--commandBar[aria-label="More options"]').click();
+      $('button[name="Get Add-ins"]').click();
       const iframe = $('iframe.fm3HSVWLq7Ufa6UsA9Ia3');
       iframe.waitForExist()
-      await browser.switchToFrame(iframe);
-      await $('#MyAddins').click();
-      await $('#PrivateAddInDropdownButton').click();
-      await $('a=Add from URL...').click();
+      browser.switchToFrame(iframe);
+      $('#MyAddins').click();
+      $('#PrivateAddInDropdownButton').click();
+      $('a=Add from URL...').click();
 
       //browser.getTitle().should.match(/BrowserStack - Google Search/i);
   });
